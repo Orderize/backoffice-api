dbd54f495a65150edc21e8c644bd50a0988ec8f6
GabrielRodAmaral <gabriel.amaral@sptech.school>
2024-08-25 15:57:10 -0300

    Initial commit

---
+++ src/main/java/com/orderize/backoffice_api/BackofficeApiApplication.java
@@ -0,0 +1,13 @@
+package com.orderize.backoffice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderize/backoffice_api/BackofficeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderize.backoffice_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

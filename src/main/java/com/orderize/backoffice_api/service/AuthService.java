2714ef13f5177dbf9dafcb1102051af30729ea97
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-10-26 22:42:57 -0300

    [FEAT] ajusting dtoAuth

--- src/main/java/com/orderize/backoffice_api/service/AuthService.java
+++ src/main/java/com/orderize/backoffice_api/service/AuthService.java
@@ -2,6 +2,7 @@ package com.orderize.backoffice_api.service;
 
 import com.orderize.backoffice_api.dto.auth.AuthenticationDto;
 import com.orderize.backoffice_api.dto.auth.LoginResponseDto;
+import com.orderize.backoffice_api.mapper.auth.AuthToResponse;
 import com.orderize.backoffice_api.model.User;
 import com.orderize.backoffice_api.repository.UserRepository;
 import com.orderize.backoffice_api.security.TokenService;
@@ -17,18 +18,22 @@ public class AuthService {
     private final UserRepository userRepository;
     private final TokenService tokenService;
     private AuthenticationManager authenticationManager;
+    private final AuthToResponse authToResponse;
 
-    public AuthService(ApplicationContext applicationContext, UserRepository userRepository, TokenService tokenService) {
+    public AuthService(ApplicationContext applicationContext, UserRepository userRepository, TokenService tokenService, AuthToResponse authToResponse) {
         this.applicationContext = applicationContext;
         this.userRepository = userRepository;
         this.tokenService = tokenService;
+        this.authToResponse = authToResponse;
     }
 
     public LoginResponseDto login( AuthenticationDto data){
         authenticationManager = applicationContext.getBean(AuthenticationManager.class);
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var auth = authenticationManager.authenticate(usernamePassword);
-        var token = tokenService.generationToken((User) auth.getPrincipal());
-        return new LoginResponseDto(token);
+        User user = (User) auth.getPrincipal();
+        var token = tokenService.generationToken(user);
+        
+        return authToResponse.map(token, user);
     }
 }

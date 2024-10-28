ebc7545715d97e3bf43f784425fc444d95bea5de
GabrielRodAmaral <gabriel.amaral@sptech.school>
2024-10-28 10:48:04 -0300

    Removing not used import

--- src/main/java/com/orderize/backoffice_api/util/observer/ObserversInitializer.java
+++ src/main/java/com/orderize/backoffice_api/util/observer/ObserversInitializer.java
@@ -2,7 +2,6 @@ package com.orderize.backoffice_api.util.observer;
 
 import com.orderize.backoffice_api.service.OrderService;
 import com.orderize.backoffice_api.service.attestation.AttestationService;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @Component

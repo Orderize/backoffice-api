d889094348c6bd8e28eab3f9c5ebda6928f53943
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-12-01 00:39:52 -0300

    insert datas in database

--- src/main/resources/db/migration/V16__create_table_order.sql
+++ src/main/resources/db/migration/V16__create_table_order.sql
@@ -13,6 +13,16 @@ CREATE TABLE orders(
 );
 
 -- just for tests (MOCK) - bad practice
-INSERT INTO orders (fk_client, fk_responsible, type, freight, estimated_time, price) VALUES
-    ( 2, 1, 'delivery', 5.50, 45, 50.00),
-    ( 1, 2, 'saloon', 5.50, 30, 75.00);
+INSERT INTO orders (fk_client, fk_responsible, type, freight, estimated_time, price, datetime)
+VALUES
+    (1, 2, 'delivery', 5.00, 30, 85.00, CURRENT_TIMESTAMP),
+    (1, 2, 'delivery', 5.00, 50, 85.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 WEEK)),
+    (1, 2, 'saloon', 6.00, 30, 95.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 WEEK)),
+    (2, 1, 'delivery', 4.50, 55, 90.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 WEEK)),
+    (2, 1, 'saloon', 8.00, 40, 120.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)),
+    (1, 2, 'delivery', 4.50, 45, 55.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)),
+    (1, 2, 'saloon', 6.50, 35, 90.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)),
+    (2, 1, 'delivery', 5.00, 60, 110.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)),
+    (1, 2, 'saloon', 5.75, 50, 95.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)),
+    (2, 1, 'delivery', 7.50, 45, 60.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH)),
+    (2, 1, 'saloon', 4.00, 40, 80.00, DATE_SUB(CURRENT_DATE, INTERVAL 1 MONTH));

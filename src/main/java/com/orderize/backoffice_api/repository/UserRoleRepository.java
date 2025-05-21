413997f52322df743080e5af7c4a257c1aab60e7
thiago <tigo@tigo3zl.tigo>
2025-05-20 21:40:59 -0300

    removendo arquivos desnecessrios

--- src/main/java/com/orderize/backoffice_api/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.orderize.backoffice_api.repository;
-
-import com.orderize.backoffice_api.model.UserRole;
-import com.orderize.backoffice_api.model.UserRoleId;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleId> {
-}

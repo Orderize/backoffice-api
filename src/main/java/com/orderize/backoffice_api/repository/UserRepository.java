93fa1afe41dd8f36d6b850f75b7275ef7d6f36f0
Ana Beatriz  Silva Oliveira <142517624+AnaOliveira051@users.noreply.github.com>
2025-06-08 22:27:01 -0300

    send-passwd-recovery (#50)
    * FEATURE: Criação de envio de email para recuperação de senha
    
    * REFACT: Refatoração do codigo
    
    * Retornando ativo flyway
    
    * REFACTOR: Refatoração da classe Email Service e Testes do EmailService
    
    * arrumando o nome do users
    
    * arrumando o nome do users
    
    * arrumando o application-test
    
    ---------
    
    Co-authored-by: Thiago Santos <134028982+Sant-Thiago@users.noreply.github.com>
    Co-authored-by: thiago <tigo@tigo3zl.tigo>

--- src/main/java/com/orderize/backoffice_api/repository/UserRepository.java
+++ src/main/java/com/orderize/backoffice_api/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.orderize.backoffice_api.repository;
 
 import com.orderize.backoffice_api.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
